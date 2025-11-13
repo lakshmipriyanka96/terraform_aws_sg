f1e5bf75e093c604d1a3e6f714e87e07ba719460
llakshmipriyanka96 <llakshmipriyanka1996@gmail.com>
2025-11-13 22:54:39 +0530

    all data

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "aws_security_group" "main" {
-  name        = var.sg_name
+  name        = var.sg_names
   description = var.sg_description
   vpc_id      = var.vpc_id
 
@@ -14,7 +14,7 @@ resource "aws_security_group" "main" {
     var.sg_tags,
     local.common_tags,
     {
-  Name = "${local.common_name_suffix}-${var.sg_name}"
+  Name = "${local.common_name_suffix}-${var.sg_names}"
     }
 )
 }
